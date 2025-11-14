6f917da522e3e6511a1e293f222080afbbf43035
Chia <angel6160@gmail.com>
2025-11-14 10:12:26 +0800

    修改js

--- JS/all.js
+++ JS/all.js
@@ -1,19 +1,8 @@
 console.log(api_path, token);
 
 const productList = document.querySelector(".productWrap");
+const productSelect = document.querySelector(".productSelect");
 let productData = [];
-let str = `<li class="productCard">
-          <h4 class="productType">新品</h4>
-          <img
-            src="https://hexschool-api.s3.us-west-2.amazonaws.com/custom/dp6gW6u5hkUxxCuNi8RjbfgufygamNzdVHWb15lHZTxqZQs0gdDunQBS7F6M3MdegcQmKfLLoxHGgV3kYunUF37DNn6coPH6NqzZwRfhbsmEblpJQLqXLg4yCqUwP3IE.png"
-            alt=""
-          />
-          <a href="#" class="addCardBtn">加入購物車</a>
-          <h3>Antony 雙人床架／雙人加大</h3>
-          <del class="originPrice">NT$18,000</del>
-          <p class="nowPrice">NT$12,000</p>
-        </li>`;
-productList.innerHTML = str;
 
 // 設定初始化
 function init() {
@@ -30,8 +19,20 @@ function getProductList() {
       productData = response.data.products;
       let str = "";
       productData.forEach(function (item) {
-        str += `<li>${item.title}</li>`;
+        str += `<li class="productCard">
+          <h4 class="productType">新品</h4>
+          <img
+            src=${item.images}
+            alt="${item.title}"
+          />
+          <a href="#" class="addCardBtn">加入購物車</a>
+          <h3>${item.title}</h3>
+          <del class="originPrice">NT$${item.origin_price}</del>
+          <p class="nowPrice">NT$${item.price}</p>
+        </li>`;
       });
       productList.innerHTML = str;
     });
 }
+
+// 設定選取區
