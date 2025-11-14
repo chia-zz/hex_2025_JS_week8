f20013e4f09c028ef8536aadbdf5216519009644
Chia <angel6160@gmail.com>
2025-11-15 00:08:09 +0800

    更新後台js

--- JS/admin.js
+++ JS/admin.js
@@ -1,19 +1,50 @@
-// C3.js
-let chart = c3.generate({
-  bindto: "#chart", // HTML 元素綁定
-  data: {
-    type: "pie",
-    columns: [
-      ["Louvre 雙人床架", 1],
-      ["Antony 雙人床架", 2],
-      ["Anty 雙人床架", 3],
-      ["其他", 4],
-    ],
-    colors: {
-      "Louvre 雙人床架": "#DACBFF",
-      "Antony 雙人床架": "#9D7FEA",
-      "Anty 雙人床架": "#5434A7",
-      其他: "#301E5F",
-    },
-  },
-});
+let orderData = [];
+const orderList = document.querySelector(".order-list");
+
+function init() {
+  getOrderList();
+}
+init();
+
+// 獲取訂單資料
+function getOrderList() {
+  axios
+    .get(
+      `https://livejs-api.hexschool.io/api/livejs/v1/admin/${api_path}/orders`,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    )
+    .then(function (response) {
+      orderData = response.data.orders;
+      console.log(orderData);
+
+      let str = "";
+      orderData.forEach(function (item) {
+        str += `<tr>
+              <td>${item.id}</td>
+              <td>
+                <p>${item.user.name}</p>
+                <p>${item.user.tel}</p>
+              </td>
+              <td>${item.user.address}</td>
+              <td>${item.user.email}</td>
+              <td>
+                <p>Louvre 雙人床架</p>
+              </td>
+              <td>${item.createdAt}</td>
+              <td class="orderStatus">
+                <a href="#">${item.paid}</a>
+              </td>
+              <td>
+                <input type="button" class="delSingleOrder-Btn" data-id="${item.id}" value="刪除" />
+              </td>
+            </tr>`;
+      });
+      orderList.innerHTML = str;
+    });
+}
+
+// 渲染訂單資料
